fc72adb3bb86b55a959567ad27e481ace788deb4
faheloDev <fahelo.dev@gmail.com>
2022-11-03 12:10:45 -0300

    Semana 6 :

---
+++ src/test/java/aut/bdd/pages/CucumberHomePage.java
@@ -0,0 +1,25 @@
+package aut.bdd.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import static framework.engine.selenium.DriverFactory.getDriver;
+
+public class CucumberHomePage {
+
+    //locators
+    By barraBusquedaLocator = By.name("q");
+    By btnBuscarConGoogleLocator = By.name("btnK");
+    By btnVoyATenerSuerteLocator = By.name("btnI");
+    String url = "https://www.google.com";
+
+    public void navegarAlHome() {
+        getDriver().navigate().to(url);
+    }
+
+    public void buscarConBotonBuscar(String tsoft) {
+        getDriver().findElement(barraBusquedaLocator).sendKeys(tsoft);
+        getDriver().findElement(barraBusquedaLocator).sendKeys(Keys.ESCAPE);
+        getDriver().findElement(btnBuscarConGoogleLocator).click();
+    }
+
+}
