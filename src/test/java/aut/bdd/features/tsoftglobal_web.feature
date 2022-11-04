01f96dbbb94fa7384d1efc922f0ed95895e74761
faheloDev <fahelo.dev@gmail.com>
2022-11-04 14:35:40 -0300

    Semana 6 : uso de clase DataTable en Cucumber

--- src/test/java/aut/bdd/features/tsoftglobal_web.feature
+++ src/test/java/aut/bdd/features/tsoftglobal_web.feature
@@ -1,4 +1,18 @@
 Feature: tsoftglobal home page website
   revisaremos algunas caracteristicas de acceso de la pagina tsoftglobal
 
-Scenario:
+  @bc10 @regresion
+  Scenario: Despliegue de paginas del boton SOLUCIONES de la barra navegadora de la home page de Tsoft
+    Given que estoy en la home page de Tsoft
+    When me posiciono en el boton soluciones
+    Then se debe desplegar la siguiente lista de Links
+      |Links a Desplegar|
+      |BUILT-IN QUALITY |
+      |BUSINESS SYSTEM TEAMS|
+      |INNOVACION DIGITAL   |
+      |OPERACION INTELIGENTE|
+      |TRANSFORMACION TECNOLOGICA|
+      |PLANIFICACION BIMODAL EMPRESARIAL|
+      |PLATFORM SYSTEM TEAMS            |
+      |VISIBILIDAD. INTELIGENCIA Y GOBIERNO|
+
--- src/test/java/aut/bdd/steps/CucumberSteps.java
+++ src/test/java/aut/bdd/steps/CucumberSteps.java
@@ -2,8 +2,12 @@ package aut.bdd.steps;
 
 import aut.bdd.pages.CucumberHomePage;
 import framework.engine.selenium.DriverFactory;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java8.En;
 import org.junit.jupiter.api.Assertions;
+
+import java.sql.SQLOutput;
+import java.util.List;
 
 public class CucumberSteps implements En {
 
@@ -36,6 +40,23 @@ public class CucumberSteps implements En {
         Then("the Breaker must guess a word with {int} characters", (Integer int1) -> {
             // Write code here that turns the phrase above into concrete actions
             Assertions.assertTrue(true);
+        });
+        Given("^que estoy en la home page de Tsoft$", () -> {
+            Assertions.assertTrue(true);
+        });
+        When("^me posiciono en el boton soluciones$", () -> {
+            Assertions.assertTrue(true);
+        });
+
+        Then("^se debe desplegar la siguiente lista de Links$", (DataTable table) -> {
+
+            //Crear una Lista de Listas : List<List<String>>
+            List<List<String>> data = table.asLists();
+            String link1 = data.get(0).get(0);
+            String valor1 = data.get(0).get(1);
+
+            System.out.println("Flag "+link1+" "+valor1);
+
         });
 
 
