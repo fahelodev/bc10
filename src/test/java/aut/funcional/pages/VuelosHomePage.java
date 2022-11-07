407974ebeabfd61d5cffe27b3b4ba9c98b38f704
namesco12 <pehuen.dias@tsoftglobal.com>
2022-11-07 14:59:54 -0300

    Se agregaron Correciones de los Test

--- src/test/java/aut/funcional/pages/VuelosHomePage.java
+++ src/test/java/aut/funcional/pages/VuelosHomePage.java
@@ -1,9 +1,10 @@
 package aut.funcional.pages;
 
 import framework.engine.selenium.SeleniumWrapper;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+
+
+import java.io.IOException;
 
 import static framework.engine.utils.Constants.BASE_URL_AUT;
 
@@ -16,86 +17,131 @@ public class VuelosHomePage extends SeleniumWrapper {
     By tbDestino= By.xpath("(//input[@placeholder='Ciudad o aeropuerto'])[2]");
     By btnBarcelona= By.xpath("//li[@data-option-index='0']");
     By btnMadrid = By.xpath("//span[@class='display-1okygcz-Autocompleter-styled']");
-    By btnCalentario = By.xpath("//button[@class='display-19sl708-DateInput-styled-DateInput-styled']");
-    By btnDesplazarCalendario = By.xpath("//button[@aria-label='Next month']");
-    By btnMes= By.xpath("//button[@class='display-silvgf-Day-styled-Day-styled-Day-styled-Day-styled']");
-    By btnBuscar= By.xpath("//button[@class='display-joffoz-Button']");
-    By btnPrecioNormal= By.xpath("(//div[@class='FullTripCard__AvailablePricesContainer-sc-z8znd4-6 ciYfDV'])[1]");
+    By btnPrecioNormal= By.xpath("(//div[normalize-space()='Precio normal'])[1]");
+    By btnPrecioNormal1= By.xpath("(//div[normalize-space()='Precio normal'])[2]");
     By btnElegirClassic = By.xpath("//button[@class='Button-sc-1bbve8d-0 jlNCZl']");
     By tiTitulo= By.xpath("//div[@class='welcome__claim-title']");
     By tbNombre= By.xpath("//input[@name='name']");
     By tbApellido= By.xpath("//input[@name='surname']");
     By tbEmail= By.xpath("//input[@id='contact-email']");
-    By tbTelefonoCar= By.xpath("//div[@title='Argentina: +54']");
     By tbTelefono = By.xpath("//input[@data-test='input-phone']");
-    By tbDireccionPostal = By.xpath("//input[@name='address']");
-    By tbCalle = By.xpath("//input[@name='houseNumber']");
-    By tbCodigoPostal = By.xpath("//input[@name='postCode']");
-    By tbCiudad = By.xpath("//input[@name='city']");
-    By liPais = By.xpath("//div[@class='selected-flag']");
-    By btnArg = By.xpath("//div[@class='flag ar']");
+
     By rbSr = By.xpath("//label[@id='radio-groups.1.travellers.1.title-MALE-label']");
-    By tbQRNombre = By.xpath("//input[@data-test='input-name']");
-    By tbQRApellido = By.xpath("//input[@name='groups.1.travellers.1.surname']");
     By tbQRDiaCumple = By.xpath("//input[@data-test='input-groups.1.travellers.1.dateOfBirth-day']");
     By liQRMesCumple = By.xpath("//option[@class='select-placeholder']");
     By btnNov = By.xpath("//option[@value='11']");
     By tbQRAnioCumple = By.xpath("//input[@placeholder='AAAA']");
-    By liTipoDocumento= By.xpath("//div[@class=' css-13wifi2']");
-    By liPasaporte = By.xpath("//option[@value='PA']");
-
+    By liTipoDocumento= By.xpath("//input[@aria-label='groups.1.travellers.1.documentType']");
+    By laErrorEmail= By.xpath("//label[normalize-space()='Introduce un email válido']");
     By tbDNI=By.xpath("//input[@name='groups.1.travellers.1.documentNumber']");
     By btnSig = By.xpath("//button[@class='btn btn-cta lead-generation-submit__btn ']");
-    By laIngreseEmail = By.xpath("(//label[@class='form-elements-2__error-label  '])[3]");
-    By laMasBarato = By.xpath("(//div[@class='sorting-tabs-view__label'][normalize-space()='Más barato'])[2]");
-    By liPrimerElemento = By.xpath("(//span[@class='TripCardPrice__FinalPrice-sc-1d8mdrx-5 rCnqw Tooltip___StyledMuiTooltip-sc-ya8k7d-3 jAgUJM'])[1]");
+    By laMasBarato = By.xpath("(//div[@class='sorting-tabs-view__amount'])[5]");
+    //By liPrimerElemento = By.xpath("(//span[@color='var(--lmn-ds-colors-primary-400)'])[1]");
+
     By btnVuelosMasBuscados = By.xpath("//a[@href='https://www.rumbo.es/es/vuelos/top-destinos.html']");
     By tiVuelosBaratos = By.xpath("//h2[normalize-space()='Vuelos baratos a las localidades más solicitadas']");
-    By btnFecha = By.xpath("//button[@class='display-silvgf-Day-styled-Day-styled-Day-styled-Day-styled']");
-    By btnNinguno = By.xpath("//span[@class='checkboxlist-filter-view__desc desc'][normalize-space()='Ninguna']");
+   By btnNinguno = By.xpath("//span[@class='checkboxlist-filter-view__desc desc'][normalize-space()='Ninguna']");
     By btnEmpresa = By.xpath("//span[@class='desc--title'][normalize-space()='Air Europa']");
-    By rbNoNecesito = By.xpath("//div[@class='css-853par']");
-    By btnSiguiente = By.xpath("//button[@class='btn btn-cta lead-generation-submit__btn ']");
-    By btnTarifaSinDescuento = By.xpath("//span[@class='sc-jhLFZp bRQiXF']");
-    By btnNoGracias = By.xpath("//div[@class='insurance__noThanks-expandable-container']");
+    By rbNoNecesito = By.xpath("(//span[@class='check'])[5]");
+    By btnTarifaSinDescuento = By.xpath("//span[normalize-space()='Tarifa sin descuento Plus']");
+    By btnNoGracias = By.xpath("//label[@class='insurance__noThanks-radio-label']");
 
     By btnBUscarVuelo= By.xpath("(//button[normalize-space()='Buscar'] )[1]");
-
+    By tbDireccionPostal=By.xpath("//input[@name='address']");
+    By tbNumeroDeCalle=By.xpath("//input[@name='houseNumber']");
+    By tbCodigoPostal=By.xpath("//input[@name='postCode']");
+    By tbCiudad=By.xpath("//input[@name='city']");
+    By rbSinEquipaje1=By.xpath("(//span[normalize-space()='Sin equipaje facturado'])[1]");
+    By rbSinEquipaje2=By.xpath("(//span[normalize-space()='Sin equipaje facturado'])[2]");
     public VuelosHomePage(WebDriver driver) {
         super(driver);
         driver.manage().window().maximize();
     }
 
-    public void navegarAlHome(){
+    public void navegarAlHome() throws InterruptedException {
         navigateTo(BASE_URL_AUT);
-
         click(btnRechazarCookies);
+        Thread.sleep(3000);
         click(btnVuelos);
         click(btnVuelosMasBuscados);
         cambiarPag();
+        scroll(600,600);
 
     }
 
     public void NavegarAHomeYCampos() throws InterruptedException {
         navigateTo(BASE_URL_AUT);
+        Thread.sleep(2000);
         click(btnRechazarCookies);
         click(btnVuelos);
-        click(btnSoloIda);
+        Thread.sleep(5000);
         click(tbOrigen);
         write("barc",tbOrigen);
+        Thread.sleep(3000);
         click(btnBarcelona);
         click(tbDestino);
         write("madr",tbDestino);
         click(btnMadrid);
+        Thread.sleep(3000);
         click(btnBUscarVuelo);
-        cambiarPag();
-        click(btnPrecioNormal);
-        Thread.sleep(7000);
+        cambiarPag(); //Demora
+        Thread.sleep(25000);
+        click(btnPrecioNormal1);
+        Thread.sleep(17000);
+        click(btnElegirClassic);
+        Thread.sleep(10000);
 
     }
 
     public void LlegarAFormulario() throws InterruptedException {
         navigateTo(BASE_URL_AUT);
+        waitElemtToBeClickable(3,btnRechazarCookies);
+        click(btnRechazarCookies);
+        click(btnVuelos);
+        Thread.sleep(10000);
+        click(tbOrigen);
+        write("barc",tbOrigen);
+        Thread.sleep(3000);
+        click(btnBarcelona);
+        click(tbDestino);
+        write("madr",tbDestino);
+        Thread.sleep(3000);
+        click(btnMadrid);
+        click(btnBUscarVuelo);
+        Thread.sleep(25000);
+        click(btnPrecioNormal);
+        Thread.sleep(25000);
+        click(btnElegirClassic);
+        Thread.sleep(25000);
+        write("Pehuen",tbNombre);
+        write("Dias",tbApellido);
+        write("fffasd@gmail.com",tbEmail);
+        write("4567183",tbTelefono);
+        click(rbSr);
+        write("23",tbQRDiaCumple);
+        click(liQRMesCumple);
+        click(btnNov);
+        write("1996",tbQRAnioCumple);
+        write("1235",tbCodigoPostal);
+        write("6371",tbDireccionPostal);
+        write("31245",tbNumeroDeCalle);
+        write("La Rioja",tbCiudad);
+        click(rbNoNecesito);
+        click(rbSinEquipaje1);
+        Thread.sleep(3000);
+        click(rbSinEquipaje2);
+        click(btnSig);
+        Thread.sleep(15000);
+        click(btnNoGracias);
+        click(btnTarifaSinDescuento);
+        click(btnSig);
+        Thread.sleep(10000);
+
+    }
+
+    public void ErrorEmail() throws InterruptedException {
+        navigateTo(BASE_URL_AUT);
+        Thread.sleep(10000);
         click(btnRechazarCookies);
         click(btnVuelos);
         Thread.sleep(10000);
@@ -109,40 +155,102 @@ public class VuelosHomePage extends SeleniumWrapper {
         Thread.sleep(3000);
         click(btnMadrid);
         click(btnBUscarVuelo);
-        Thread.sleep(20000);
+        Thread.sleep(10000);
         click(btnPrecioNormal);
-        click(tbNombre);
+        Thread.sleep(15000);
+        click(btnElegirClassic);
+        Thread.sleep(10000);
+        write("Pehuen",tbNombre);
+        write("Dias",tbApellido);
+        write("4567183",tbTelefono);
+        click(btnSig);
+        Thread.sleep(10000);
+    }
+
+    public void VueloBarato() throws InterruptedException {
+        navigateTo(BASE_URL_AUT);
+        Thread.sleep(10000);
+        click(btnRechazarCookies);
+        click(btnVuelos);
+        Thread.sleep(10000);
+        click(btnSoloIda);
+        click(tbOrigen);
+        write("barc",tbOrigen);
+        Thread.sleep(3000);
+        click(btnBarcelona);
+        click(tbDestino);
+        write("madr",tbDestino);
+        Thread.sleep(3000);
+        click(btnMadrid);
+        click(btnBUscarVuelo);
+        Thread.sleep(15000);
+        click(laMasBarato);
+        Thread.sleep(15000);
+    }
+
+    public void VueloDirecto() throws InterruptedException {
+        navigateTo(BASE_URL_AUT);
+        click(btnRechazarCookies);
+        click(btnVuelos);
+        Thread.sleep(10000);
+        click(tbOrigen);
+        write("barc",tbOrigen);
+        Thread.sleep(3000);
+        click(btnBarcelona);
+        click(tbDestino);
+        write("madr",tbDestino);
+        Thread.sleep(3000);
+        click(btnMadrid);
+        click(btnBUscarVuelo);
+        Thread.sleep(35000);
+        click(btnEmpresa);
+        Thread.sleep(8000);
+        click(btnNinguno);
+        Thread.sleep(8000);
+        click(btnPrecioNormal);
+        Thread.sleep(25000);
+        click(btnElegirClassic);
         write("Pehuen",tbNombre);
         write("Dias",tbApellido);
         write("fffasd@gmail.com",tbEmail);
         write("4567183",tbTelefono);
-        write("5400",tbDireccionPostal);
-        write("123",tbCalle);
-        write("3455",tbCodigoPostal);
-        write("La Rioja",tbCiudad);
-        click(rbSr);
         write("23",tbQRDiaCumple);
         click(liQRMesCumple);
+        click(rbSr);
         click(btnNov);
         write("1996",tbQRAnioCumple);
-        click(liTipoDocumento);
-        click(liPasaporte);
-        write("341513452",tbDNI);
-        click(btnNoGracias);
+        //Reg Error
+        click(rbNoNecesito);
         click(btnSig);
-
-
+        Thread.sleep(10000);
+        click(btnNoGracias);
+        click(btnTarifaSinDescuento);
+        Thread.sleep(10000);
+        click(btnSig);
+        Thread.sleep(24000);
     }
 
-
-
-    public String TituloTest5(){
-        return getText(tiVuelosBaratos);
-    }
 
     public String TituloTest1(){
         return getText(tiTitulo);
     }
+    public String TituloTest2(){
+        return getText(tiTitulo);
+    }
+    public String TituloTest3(){
+        return getText(laErrorEmail);
+    }
+
+
+    public String TituloTest5() {return getText(tiVuelosBaratos);}
+    public String TituloTest6(){
+        return getText(tiTitulo);
+    }
+
+    public void screenShot() throws IOException {
+        capturaPantalla();
+    }
+
 
 
 }
